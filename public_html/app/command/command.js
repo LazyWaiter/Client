514e3aa04ba6991c1edbdff4316ed420b7dd0221
Raffiou <Raffiou@Raffiou-PC>
2014-12-21 19:07:56 +0100

    Initialisation cadre de travail LazyWaiter/Client

---
+++ public_html/app/command/command.js
@@ -0,0 +1,28 @@
+'use strict';
+
+angular.module('myApp.command', ['ngRoute'])
+
+.controller('CommandCtrl',['$scope',function($scope) {
+        
+        $scope.tables = [
+            {id:"101",name:"Everest"},
+            {id:"102",name:"Mont-Blanc"},
+            {id:"103",name:"Kilimandjaro"}
+        ];
+        
+        $scope.selectedTable = {value : $scope.tables[0]};
+        
+        $scope.cocktails = [
+            {id:"201",name:"Grzaniec"},
+            {id:"202",name:"Glögi"},
+            {id:"203",name:"Gimlet"}
+        ];
+        
+        $scope.selectedCocktail = {value:$scope.cocktails[0]};
+        
+        /* utiliser variables ci dessous pour effectuer enregistrement en bdd */
+        $scope.sendCommand = function(){
+            console.log($scope.selectedTable);
+            console.log($scope.selectedCocktail);
+        };
+}]);
